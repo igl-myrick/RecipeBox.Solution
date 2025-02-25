2f8d76544fe8964930d1cdd18beae8948b022088
igl-myrick <igl.myrick@gmail.com>
2025-02-25 15:00:18 -0800

    change type of Rating from int to string

--- RecipeBox/Models/Recipe.cs
+++ RecipeBox/Models/Recipe.cs
@@ -8,10 +8,12 @@ namespace RecipeBox.Models
     public int RecipeId { get; set; }
     [Required(ErrorMessage = "Please enter a valid name.")]
     public string Name { get; set; }
+    [Required(ErrorMessage = "Please add some ingredients first.")]
     public string Ingredients { get; set; }
+    [Required(ErrorMessage = "Please enter some instructions first.")]
     public string Instructions { get; set; }
     [Range(1, 5)]
-    public int Rating { get; set; }
+    public string Rating { get; set; }
     public List<RecipeTag> JoinEntities { get; }
   }
 }
